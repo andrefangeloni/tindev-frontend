7d90fa15d4634734337302e7950929f44677bdcd
andrefangeloni <andrefangeloni@gmail.com>
2019-08-07 10:28:14 -0300

    Recebendo dados da API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import "./Login.css";
+import api from "../services/api";
 import logo from "../assets/logo.svg";
 
 export default function Login({ history }) {
   const [username, setUsername] = useState("");
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    history.push("/main");
+
+    const response = await api.post("/devs", {
+      username
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
   }
 
   return (
