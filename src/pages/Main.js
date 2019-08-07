bfe9de8fba6db0b59cde88b5fc07bb3761c729b6
andrefangeloni <andrefangeloni@gmail.com>
2019-08-07 11:07:13 -0300

    Main estilizada

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,91 @@
 import React from "react";
+import "./Main.css";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import logo from "../assets/logo.svg";
 
-// import { Container } from './styles';
+export default function Main({ match }) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="Diego Fernandes"
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>CTO na Rocketseat</p>
+          </footer>
 
-export default function Main() {
-  return <h1>Hello Main</h1>;
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="Diego Fernandes"
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>CTO na Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="Diego Fernandes"
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>CTO na Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="Diego Fernandes"
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>CTO na Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
